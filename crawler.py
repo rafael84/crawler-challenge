38d100719b7e61c777c00d93496a8e5b3ddff200
Rafael Martins <rafael84@gmail.com>
2014-08-29 00:55:33 -0300

    change crawler to extract product data

--- crawler.py
+++ crawler.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python
 import re
 import logging
-import urllib2
-
-from StringIO import StringIO
-import gzip
+import requests
+from pprint import pprint
 
 from BeautifulSoup import BeautifulStoneSoup as Soup
 
-logging.basicConfig(level=logging.INFO)
+
+logging.basicConfig(
+    format='%(asctime)s %(name)s %(levelname)s %(message)s',
+    level=logging.WARNING
+)
 
 ROOT_URL = 'http://epocacosmeticos.com.br/sitemap.xml'
-PRODUCTS_REGEX = r'sitemap-produtos.*\.xml'
+PRODUCTS_REGEX = re.compile(r'sitemap-produtos.*\.xml')
+PRODUCT_NAME_REGEX = re.compile(r'\bproductName\b')
 
 
-def make_request(url):
+def make_request(url, allow_redirects=False):
     logging.info('making a request to [%s]' % url)
-    request = urllib2.Request(url)
-    request.add_header('Accept-encoding', 'gzip')
-    response = urllib2.urlopen(request)
-    if response.info().get('Content-Encoding') == 'gzip':
-        buf = StringIO(response.read())
-        f = gzip.GzipFile(fileobj=buf)
-        data = f.read()
+    response = requests.get(url, allow_redirects=allow_redirects)
+    if (not allow_redirects) and (response.status_code in (301, 302)):
+        data = None
     else:
-        data = response.read()
+        data = response.content
     logging.debug('data: %s' % data)
     return data
 
@@ -32,7 +31,7 @@ def make_request(url):
 def extract_product_sitemaps(response):
     logging.info('extracting product sitemaps...')
     soup = Soup(response)
-    return soup.findAll('loc', text=re.compile(PRODUCTS_REGEX))
+    return soup.findAll('loc', text=PRODUCTS_REGEX)
 
 
 def extract_product_urls(url):
@@ -44,17 +43,37 @@ def extract_product_urls(url):
         yield url.text
 
 
-if __name__ == '__main__':
+def grab_product_details(url):
+    logging.info('grabbing product details [%s]' % url)
+    response = make_request(url, False)
+    data = None
+    if response is not None:
+        logging.info('found product url [%s]' % url)
+        soup = Soup(response)
+        page_title = soup.title.string
+        product_name = soup(attrs={'class': PRODUCT_NAME_REGEX})[0].string
+        data = {
+            'page_url': url,
+            'page_title': page_title,
+            'product_name': product_name
+        }
+    else:
+        logging.warning('skipping product url [%s]' % url)
+    return data
+
+
+def main():
     logging.info('crawler has been started')
-    product_sitemaps = extract_product_sitemaps(make_request(ROOT_URL))
+    response = make_request(ROOT_URL, True)
+    product_sitemaps = extract_product_sitemaps(response)
 
-    products = []
-    for url in product_sitemaps:
-        products.extend(list(extract_product_urls(url)))
-
-    from pprint import pprint
-    pprint(products)
-
-    logging.info('COUNT: %d' % len(products))
+    for sitemap_url in product_sitemaps:
+        for product_url in extract_product_urls(sitemap_url):
+            data = grab_product_details(product_url)
+            pprint(data)
 
     logging.info('crawler finished')
+
+
+if __name__ == '__main__':
+    main()
