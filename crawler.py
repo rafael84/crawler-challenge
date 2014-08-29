913f2c6d444579054af26fd50a36017e52fe7728
Rafael Martins <rafael84@gmail.com>
2014-08-28 23:40:55 -0300

    add logic to extract product urls from sitemap

--- crawler.py
+++ crawler.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
+import re
 import logging
 import urllib2
 
 from StringIO import StringIO
 import gzip
 
+from BeautifulSoup import BeautifulStoneSoup as Soup
+
 logging.basicConfig(level=logging.INFO)
 
-ROOT_URL = 'http://epocacosmeticos.com.br'
+ROOT_URL = 'http://epocacosmeticos.com.br/sitemap.xml'
+PRODUCTS_REGEX = r'sitemap-produtos.*\.xml'
 
 
 def make_request(url):
@@ -25,7 +29,32 @@ def make_request(url):
     return data
 
 
+def extract_product_sitemaps(response):
+    logging.info('extracting product sitemaps...')
+    soup = Soup(response)
+    return soup.findAll('loc', text=re.compile(PRODUCTS_REGEX))
+
+
+def extract_product_urls(url):
+    logging.info('extracting product urls from [%s]...' % url)
+    response = make_request(url)
+    soup = Soup(response)
+    loc = soup.findAll('loc')
+    for url in loc:
+        yield url.text
+
+
 if __name__ == '__main__':
     logging.info('crawler has been started')
-    # todo
+    product_sitemaps = extract_product_sitemaps(make_request(ROOT_URL))
+
+    products = []
+    for url in product_sitemaps:
+        products.extend(list(extract_product_urls(url)))
+
+    from pprint import pprint
+    pprint(products)
+
+    logging.info('COUNT: %d' % len(products))
+
     logging.info('crawler finished')
